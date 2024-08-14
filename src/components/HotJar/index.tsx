43bdee8d50aa007ca364161ff60349278f1d394a
Shawn V <shawn@fiskl.com>
2024-08-14 11:50:31 +0100

    updated hot jar

--- src/components/HotJar/index.tsx
+++ src/components/HotJar/index.tsx
@@ -5,25 +5,30 @@ const HotJar: React.FC = () => {
   const isBrowser = useIsBrowser();
 
   React.useEffect(() => {
+    console.log('Current NODE_ENV:', process.env.NODE_ENV);
+    console.log('Current Browser Mode:', isBrowser);
+
     if (isBrowser && process.env.NODE_ENV === 'production') {
       const script = document.createElement('script');
       script.innerHTML = `
-          (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:5094557,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+        (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:5094557,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
       `;
-      document.body.appendChild(script);
+      document.head.appendChild(script);
       
       return () => {
-        document.body.removeChild(script);
+        document.head.removeChild(script);
       };
     }
   }, [isBrowser]);
 
   return null;
 };
+
+export default HotJar;
